284991aca55c39a933d6e8402e98aa39bac14d8d
CodeManBist <sagarbist372@gmail.com>
2026-04-21 23:24:05 +0530

    Initial setup with server running + mongoDB atlas connection db

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
+
